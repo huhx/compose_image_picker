e2acc146b90232d74e2d187ef089503a82e28efe
hongxiang <huxhu@thoughtworks.com>
2022-07-24 18:12:53 +0800

    feat: implement the navigation between Home and DropDown

--- compose_image_picker/src/main/java/com/huhx/picker/view/AssetPicker.kt
+++ compose_image_picker/src/main/java/com/huhx/picker/view/AssetPicker.kt
@@ -1,5 +1,7 @@
 package com.huhx.picker.view
 
+import android.app.Activity
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +19,8 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,8 +30,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -40,6 +46,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
@@ -47,32 +60,96 @@ import com.google.accompanist.pager.rememberPagerState
 import com.huhx.picker.R
 import com.huhx.picker.constant.showShortToast
 import kotlinx.coroutines.launch
+import kotlin.math.exp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
-fun QQAssetPicker() {
+fun QQAssetPicker(
+    onPicked: (List<String>) -> Unit
+) {
+    val expanded = remember { mutableStateOf(false) }
+    val folderName = remember { mutableStateOf("所有项目") }
+    val navController = rememberAnimatedNavController()
+    val isHome = currentRoute(navController) == "home"
+
     Scaffold(
-        topBar = { TopAppBar() }
+        topBar = {
+            TopAppBar(
+                expanded = expanded,
+                folderName = folderName,
+                navigateUp = {
+                    if (isHome) {
+                        onPicked(listOf("abc", "efg"))
+                    } else {
+                        navController.navigateUp()
+                    }
+                },
+                navigateToDropDown = {
+                    navController.navigate("dropDown?directory=$it")
+                },
+                onPicked = { onPicked(it) }
+            )
+        }
     ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
-            TabView()
+            AssetPickerRoute(navController = navController) {
+                navController.navigateUp()
+                expanded.value = !expanded.value
+                if (folderName.value != it) {
+                    folderName.value = it
+                }
+            }
         }
     }
 }
 
 @Composable
-fun TopAppBar() {
-    val folderName = remember { mutableStateOf("所有项目") }
+fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AssetPickerRoute(
+    navController: NavHostController,
+    navigateBack: (String) -> Unit
+) {
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            TabView()
+        }
+
+        composable(
+            "dropDown?directory={directory}",
+            arguments = listOf(navArgument("directory") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val arguments = backStackEntry.arguments!!
+            val directory = arguments.getString("directory")!!
+            FolderDropDown(directory) { navigateBack(it) }
+        }
+    }
+}
+
+@Composable
+fun TopAppBar(
+    expanded: MutableState<Boolean>,
+    folderName: MutableState<String>,
+    navigateUp: () -> Unit,
+    navigateToDropDown: (String) -> Unit,
+    onPicked: (List<String>) -> Unit
+) {
     val selectedCount = remember { mutableStateOf(2) }
     val isEnable = selectedCount.value > 0
     val text = if (isEnable) "确定(${selectedCount.value}/9)" else "确定"
 
-    val context = LocalContext.current
-
     CenterAlignedTopAppBar(
         modifier = Modifier.statusBarsPadding(),
         navigationIcon = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { navigateUp() }) {
                 Icon(
                     Icons.Filled.Close,
                     tint = Color.Black,
@@ -80,14 +157,32 @@ fun TopAppBar() {
                 )
             }
         },
-        title = { Text(folderName.value, fontSize = 16.sp) },
+        title = {
+            Row(
+                modifier = Modifier.clickable {
+                    expanded.value = !expanded.value
+                    if (expanded.value) {
+                        navigateToDropDown(folderName.value)
+                    } else {
+                        navigateUp()
+                    }
+                }
+            ) {
+                Text(folderName.value, fontSize = 18.sp)
+                if (expanded.value) {
+                    Icon(Icons.Default.KeyboardArrowUp, "")
+                } else {
+                    Icon(Icons.Default.KeyboardArrowDown, "")
+                }
+            }
+        },
         actions = {
             Button(
                 modifier = Modifier.defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
                 enabled = isEnable,
                 shape = RoundedCornerShape(5.dp),
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-                onClick = { context.showShortToast("finish selecting") }
+                onClick = { onPicked(listOf("abc", "efg")) }
             ) {
                 Text(text)
             }
