08207202ccc1d60c4c661393bbce2f73966bf705
hongxiang <huxhu@thoughtworks.com>
2022-07-25 17:59:46 +0800

    feat: create AssetLoader to load images and videos

--- compose_image_picker/src/main/java/com/huhx/picker/data/AssetViewModelFactory.kt
+++ compose_image_picker/src/main/java/com/huhx/picker/data/AssetViewModelFactory.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.ViewModelProvider
 import com.huhx.picker.constant.AssetPickerConfig
 
 class AssetViewModelFactory(
-    private val assetPickerConfig: AssetPickerConfig
+    private val assetPickerRepository: AssetPickerRepository,
+    private val assetPickerConfig: AssetPickerConfig,
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(AssetViewModel::class.java)) {
-            AssetViewModel(this.assetPickerConfig) as T
+            AssetViewModel(assetPickerRepository, assetPickerConfig) as T
         } else {
             throw IllegalArgumentException("ViewModel is Missing")
         }
