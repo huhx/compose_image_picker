7e0658afc2e75a41cdf42e7cad5ebeeb14ecfa20
hongxiang <gohuhx@163.com>
2022-07-26 21:24:35 +0800

    refactor: separate the AssetAppBar and AssetTabView

---
+++ compose_image_picker/src/main/java/com/huhx/picker/view/AssetAppBar.kt
@@ -0,0 +1,102 @@
+package com.huhx.picker.view
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.huhx.picker.data.AssetInfo
+import com.huhx.picker.data.AssetViewModel
+import com.huhx.picker.data.currentRoute
+
+@Composable
+internal fun AssetTopBar(
+    viewModel: AssetViewModel,
+    isHome: Boolean,
+    onPicked: (List<AssetInfo>) -> Unit,
+    navController: NavHostController
+) {
+    val isPreview = currentRoute(navController)?.startsWith("preview")
+    if (isPreview != true) {
+        TopAppBar(
+            viewModel = viewModel,
+            navigateUp = {
+                if (isHome) {
+                    onPicked(viewModel.selectedList)
+                } else {
+                    navController.navigateUp()
+                }
+            },
+            navigateToDropDown = {
+                navController.navigate("dropDown?directory=$it")
+            },
+            onPicked = { onPicked(it) }
+        )
+    }
+}
+
+@Composable
+fun TopAppBar(
+    viewModel: AssetViewModel,
+    navigateUp: () -> Unit,
+    navigateToDropDown: (String) -> Unit,
+    onPicked: (List<AssetInfo>) -> Unit
+) {
+    val directory = viewModel.directory.value
+    val expanded = viewModel.expanded
+
+    CenterAlignedTopAppBar(
+        modifier = Modifier.statusBarsPadding(),
+        navigationIcon = {
+            IconButton(onClick = { navigateUp() }) {
+                Icon(
+                    Icons.Filled.Close,
+                    tint = Color.Black,
+                    contentDescription = "",
+                )
+            }
+        },
+        title = {
+            Row(
+                modifier = Modifier.clickable {
+                    expanded.value = !expanded.value
+                    if (expanded.value) navigateToDropDown(directory) else navigateUp()
+                }
+            ) {
+                Text(directory, fontSize = 18.sp)
+                if (expanded.value) {
+                    Icon(Icons.Default.KeyboardArrowUp, "")
+                } else {
+                    Icon(Icons.Default.KeyboardArrowDown, "")
+                }
+            }
+        },
+        actions = {
+            Button(
+                modifier = Modifier.defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
+                enabled = viewModel.isEnable,
+                shape = RoundedCornerShape(5.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                onClick = { onPicked(viewModel.selectedList) }
+            ) {
+                Text(viewModel.selectedText)
+            }
+        }
+    )
+}
---
+++ compose_image_picker/src/main/java/com/huhx/picker/view/AssetTabView.kt
@@ -0,0 +1,96 @@
+package com.huhx.picker.view
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+import com.huhx.picker.R
+import com.huhx.picker.data.AssetViewModel
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabView(
+    viewModel: AssetViewModel,
+) {
+    val tabs = listOf(TabItem.All, TabItem.Video, TabItem.Image)
+    val pagerState = rememberPagerState()
+    Column {
+        AssetTab(tabs = tabs, pagerState = pagerState)
+        TabsContent(
+            tabs = tabs,
+            pagerState = pagerState,
+            viewModel = viewModel
+        )
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun AssetTab(
+    tabs: List<TabItem>,
+    pagerState: PagerState,
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        contentColor = Color.Black,
+        indicator = {},
+    ) {
+        tabs.forEachIndexed { index, tab ->
+            Tab(
+                selected = pagerState.currentPage == index,
+                text = { Text(text = stringResource(tab.resourceId)) },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Gray,
+                onClick = {
+                    coroutineScope.launch { pagerState.animateScrollToPage(index) }
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabsContent(
+    tabs: List<TabItem>,
+    pagerState: PagerState,
+    viewModel: AssetViewModel
+) {
+    HorizontalPager(
+        state = pagerState,
+        count = tabs.size,
+        userScrollEnabled = true
+    ) { page ->
+        tabs[page].screen(viewModel)
+    }
+}
+
+sealed class TabItem(
+    @StringRes val resourceId: Int,
+    val screen: @Composable (AssetViewModel) -> Unit
+) {
+    object All : TabItem(R.string.tab_item_all, { viewModel ->
+        AssetAll(viewModel)
+    })
+
+    object Video : TabItem(R.string.tab_item_video, { viewModel ->
+        AssetVideo(viewModel)
+    })
+
+    object Image : TabItem(R.string.tab_item_image, { viewModel ->
+        AssetImage(viewModel)
+    })
+}
