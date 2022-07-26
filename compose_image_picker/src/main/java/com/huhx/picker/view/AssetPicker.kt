548b76ac5005506174e18208cdb5afca5263f12c
hongxiang <huxhu@thoughtworks.com>
2022-07-26 13:36:29 +0800

    feat: separate the AppBar and TabView from AssetPicker

--- compose_image_picker/src/main/java/com/huhx/picker/view/AssetPicker.kt
+++ compose_image_picker/src/main/java/com/huhx/picker/view/AssetPicker.kt
@@ -1,44 +1,16 @@
 package com.huhx.picker.view
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.PagerState
-import com.google.accompanist.pager.rememberPagerState
 import com.huhx.picker.constant.AssetPickerConfig
 import com.huhx.picker.data.AssetInfo
 import com.huhx.picker.data.AssetPickerRepository
@@ -46,7 +18,6 @@ import com.huhx.picker.data.AssetPickerRoute
 import com.huhx.picker.data.AssetViewModel
 import com.huhx.picker.data.AssetViewModelFactory
 import com.huhx.picker.data.currentRoute
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
@@ -85,142 +56,5 @@ fun QQAssetPicker(
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun AssetTopBar(
-    viewModel: AssetViewModel,
-    isHome: Boolean,
-    onPicked: (List<AssetInfo>) -> Unit,
-    navController: NavHostController
-) {
-    val isPreview = currentRoute(navController)?.startsWith("preview")
-    if (isPreview != true) {
-        TopAppBar(
-            viewModel = viewModel,
-            navigateUp = {
-                if (isHome) {
-                    onPicked(viewModel.selectedList)
-                } else {
-                    navController.navigateUp()
-                }
-            },
-            navigateToDropDown = {
-                navController.navigate("dropDown?directory=$it")
-            },
-            onPicked = { onPicked(it) }
-        )
-    }
-}
-
-
-@Composable
-fun TopAppBar(
-    viewModel: AssetViewModel,
-    navigateUp: () -> Unit,
-    navigateToDropDown: (String) -> Unit,
-    onPicked: (List<AssetInfo>) -> Unit
-) {
-    val folderName = viewModel.folderName.value
-    val expanded = viewModel.expanded
-
-    CenterAlignedTopAppBar(
-        modifier = Modifier.statusBarsPadding(),
-        navigationIcon = {
-            IconButton(onClick = { navigateUp() }) {
-                Icon(
-                    Icons.Filled.Close,
-                    tint = Color.Black,
-                    contentDescription = "",
-                )
-            }
-        },
-        title = {
-            Row(
-                modifier = Modifier.clickable {
-                    expanded.value = !expanded.value
-                    if (expanded.value) navigateToDropDown(folderName) else navigateUp()
-                }
-            ) {
-                Text(folderName, fontSize = 18.sp)
-                if (expanded.value) {
-                    Icon(Icons.Default.KeyboardArrowUp, "")
-                } else {
-                    Icon(Icons.Default.KeyboardArrowDown, "")
-                }
-            }
-        },
-        actions = {
-            Button(
-                modifier = Modifier.defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
-                enabled = viewModel.isEnable,
-                shape = RoundedCornerShape(5.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-                onClick = { onPicked(viewModel.selectedList) }
-            ) {
-                Text(viewModel.selectedText)
-            }
-        }
-    )
-}
-
-@OptIn(ExperimentalPagerApi::class)
-@Composable
-fun TabView(
-    viewModel: AssetViewModel,
-) {
-    val tabs = listOf(TabItem.All, TabItem.Video, TabItem.Image)
-    val pagerState = rememberPagerState()
-    Column {
-        Tab(tabs = tabs, pagerState = pagerState)
-        TabsContent(
-            tabs = tabs,
-            pagerState = pagerState,
-            viewModel = viewModel
-        )
-    }
-}
-
-@OptIn(ExperimentalPagerApi::class)
-@Composable
-private fun Tab(
-    tabs: List<TabItem>,
-    pagerState: PagerState,
-) {
-    val coroutineScope = rememberCoroutineScope()
-
-    TabRow(
-        selectedTabIndex = pagerState.currentPage,
-        contentColor = Color.Black,
-        indicator = {},
-    ) {
-        tabs.forEachIndexed { index, tab ->
-            Tab(
-                selected = pagerState.currentPage == index,
-                text = { Text(text = stringResource(tab.resourceId)) },
-                selectedContentColor = Color.Black,
-                unselectedContentColor = Color.Gray,
-                onClick = {
-                    coroutineScope.launch { pagerState.animateScrollToPage(index) }
-                }
-            )
-        }
-    }
-}
-
-@OptIn(ExperimentalPagerApi::class)
-@Composable
-fun TabsContent(
-    tabs: List<TabItem>,
-    pagerState: PagerState,
-    viewModel: AssetViewModel
-) {
-    HorizontalPager(
-        state = pagerState,
-        count = tabs.size,
-        userScrollEnabled = true
-    ) { page ->
-        tabs[page].screen(viewModel)
     }
 }
