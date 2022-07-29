bc9984b42c74a8cc8300b50e4e1d85f035f5de72
hongxiang <gohuhx@163.com>
2022-07-29 19:19:21 +0800

    feat: add some preview for testing

--- app/src/main/java/com/huhx/picker/MainActivity.kt
+++ app/src/main/java/com/huhx/picker/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -117,5 +131,71 @@ fun GifTextPreview() {
             )
         }
     }
+}
 
+@Preview
+@Composable
+fun VideoPreview() {
+    Box(
+        modifier = Modifier.background(
+            brush = Brush.verticalGradient(
+                colors = listOf(Color(0F, 0F, 0F, 0.4F), Color(0F, 0F, 0F, 0.6F))
+            )
+        )
+    ) {
+        Text(
+            modifier = Modifier.padding(bottom = 10.dp, end = 8.dp),
+            text = "00:34",
+            color = Color.White,
+            fontSize = 14.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ImageItemPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black),
+        contentAlignment = Alignment.BottomStart
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.app_icon_foreground),
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Fit,
+            contentDescription = ""
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Red)
+                .padding(horizontal = 10.dp, vertical = 16.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(selected = false, onClick = { /*TODO*/ })
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "选择", color = Color.White)
+                }
+                Button(
+                    modifier = Modifier.defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
+                    enabled = true,
+                    shape = RoundedCornerShape(5.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                    onClick = { }
+                ) {
+                    Text("确定", color = Color.White)
+                }
+            }
+        }
+    }
 }
